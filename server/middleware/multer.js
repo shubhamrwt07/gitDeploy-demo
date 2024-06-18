eec0dc7497fc9bfb72f9a5b629ea5b7f59fed9ac
shubhamrwt07 <shubhamrwt789@gmail.com>
2024-06-18 18:38:45 +0530

    changes

--- server/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-const multer = require('multer');
-const path = require('path');
-
-
-
-const images = multer.diskStorage({});
-const imagesUpload = multer({ storage: images });
-
-module.exports = { imagesUpload };
--- server/routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-const categoryController = require("../controller/category.controller");
-
-module.exports = function Route(app) {
-    app.post("/api/category", categoryController.createCategory);
-    app.get("/api/getCategory", categoryController.getAllCategories);
-    app.delete("/api/category/:id", categoryController.deleteCategory);
-    app.put("/api/category/:id", categoryController.editCategory);
-    app.patch("/api/category/:id/status", categoryController.updateCategoryStatus);
-};
